b5163f66dcac293af5fc9dc5aeebc22f855cd18f
gunnar67 <gunnar67@aa707601-d0d4-0310-b23e-b68f20a03f4d>
2005-01-30 20:57:44 +0000

    on wxmac a wxDataFormat string may have only 4 chars

--- trunk/wxMusik/src/DNDHelper.h
+++ trunk/wxMusik/src/DNDHelper.h
@@ -1,9 +1,13 @@
 #ifndef DNDHELPER_H
 #define DNDHELPER_H
 
+#ifdef __WXMAC__// on mac the custom string may have only up to 4 chars
+#define MUSIK_SOURCES_DATAID wxT("wMSd")
+#define MUSIK_SONGLIST_DATAID wxT("wMSl")
+#else
 #define MUSIK_SOURCES_DATAID wxT("wxMusik/SourcesData")
 #define MUSIK_SONGLIST_DATAID wxT("wxMusik/SonglistData")
-
+#endif
 class CMusikInternalDataObject : public wxTextDataObject
 {
 public:

6f61545bdfba30985f091420b7bbb7a53a4e4e8e
gunnar67 <gunnar67@aa707601-d0d4-0310-b23e-b68f20a03f4d>
2005-12-27 00:24:44 +0000

    revert last change, it was totally wrong and done by mistake

--- trunk/wxMusik/MUSIKEngine/FMODExEngine/src/fmodexengine.cpp
+++ trunk/wxMusik/MUSIKEngine/FMODExEngine/src/fmodexengine.cpp
@@ -112,10 +112,11 @@ MUSIKEngine::Error FMODExEngine::_Init(int idOutput ,int idDevice,int nMixRate,i
     // initialize system
     int oldNumOutCh=2,oldNumInCh=8;
     FMOD_SOUND_FORMAT oldFormat = FMOD_SOUND_FORMAT_PCM16;
+    FMOD_DSP_RESAMPLER resamplemethod;
     if(nMixRate)
     {
-        m_pSystem->getSoftwareFormat(NULL,&oldFormat,&oldNumOutCh,&oldNumInCh,NULL);
-        if(m_pSystem->setSoftwareFormat(nMixRate,oldFormat,oldNumOutCh,oldNumInCh) != FMOD_OK)
+        m_pSystem->getSoftwareFormat(NULL,&oldFormat,&oldNumOutCh,&oldNumInCh,&resamplemethod,NULL);
+        if(m_pSystem->setSoftwareFormat(nMixRate,oldFormat,oldNumOutCh,oldNumInCh,resamplemethod) != FMOD_OK)
             return errUnknown;
     }
 
@@ -203,7 +204,7 @@ MUSIKEngine::Error FMODExEngine::EnumFrequencies(IEnumNames * pen) const
 
 MUSIKEngine::Error FMODExEngine::SetNetworkProxy(const char * s)
 {
-  return m_pSystem->setProxy(s) == FMOD_OK ? errSuccess:errUnknown;
+  return m_pSystem->setNetworkProxy(s) == FMOD_OK ? errSuccess:errUnknown;
 }
 
 MUSIKEngine::Error FMODExEngine::SetNetBuffer(int nBufferSize,int nPreBufferPercent,int nReBufferPercent)
@@ -249,11 +250,11 @@ bool FMODExEngine::SetPlayState( MUSIKEngine::PlayState state)
 	switch (state )
 	{
 	case MUSIKEngine::Paused:
-		return chgroup->setPaused(true) == FMOD_OK;
+		return chgroup->overridePaused(true) == FMOD_OK;
 	case MUSIKEngine::Playing:
-		return chgroup->setPaused(false) == FMOD_OK;
+		return chgroup->overridePaused(false) == FMOD_OK;
 	case MUSIKEngine::Stopped:
-		return chgroup->stop() == FMOD_OK;
+		return chgroup->overridePaused(true) == FMOD_OK;
 	case MUSIKEngine::Invalid:
 		return false;
 	}
